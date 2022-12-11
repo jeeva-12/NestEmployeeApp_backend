34b441458fdf8670419a134fe3880bcb85a80870
jeeva-12 <112372759+jeeva-12@users.noreply.github.com>
2022-12-11 22:03:56 +0530

    jhg

--- src/main/java/com/example/NestEmployee_backend/controller/EmployeeController.java
+++ src/main/java/com/example/NestEmployee_backend/controller/EmployeeController.java
@@ -18,26 +18,26 @@ public class EmployeeController {
 
 
     @CrossOrigin(origins = "*")
-    @PostMapping(path = "/addEmp",consumes ="application/json",produces = "application/json" )
-    public HashMap<String,String> AddEmp(@RequestBody Employee e){
-        edao.save(e);
-        HashMap<String,String> status=new HashMap<>();
-        status.put("status","success");
-        return status;
+    @PostMapping(path = "/userLogin",consumes ="application/json",produces = "application/json" )
+    public HashMap<String,String> UserLogin(@RequestBody Employee e){
+        List<Employee> employees=(List<Employee>) edao.UserLogin(e.getUsername(),e.getPassword());
+        HashMap<String,String> h=new HashMap<>();
+        if (employees.size()==0){
+            h.put("status","failed");
+        }else {
+            h.put("status","success");
+            h.put("userId",String.valueOf(employees.get(0)));
+        }
+        return h;
+
     }
 
     @CrossOrigin(origins = "*")
-    @PostMapping(path = "/emplogin",consumes = "application/json",produces = "application/json")
-    public HashMap<String,String> UserLogin(@RequestBody Employee e){
-        List<Employee> employees=(List<Employee>) edao.UserLogin(e.getUsername(),e.getPassword());
-        HashMap<String,String> map=new HashMap<>();
-        if (employees.size()==0){
-            map.put("status","failed");
-        }else {
-            map.put("status","success");
-        }
-        return map;
+    @PostMapping("/getEmp")
+    public List<Employee> GetEmployee(){
+        return (List<Employee>) edao.findAll();
     }
+
 
 
     @CrossOrigin(origins = "*")
@@ -45,16 +45,6 @@ public class EmployeeController {
     public List<Employee> SearchEmp(@RequestBody Employee e){
         return (List<Employee>) edao.SearchEmp(e.getEname());
     }
-
-    @CrossOrigin(origins = "*")
-    @GetMapping("/listEmployee")
-    public List<Employee> EmployeeList(){
-        return (List<Employee>) edao.findAll();
-    }
-
-
-
-
 
 
 }
